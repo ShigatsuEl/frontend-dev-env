99dd74156bb1d358b9011026d76bdbe35a07af0a
MinChan Lee <shigatsu970704@gmail.com>
2021-02-09 16:17:34 +0900

    18.Hot Loading Trainning

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,29 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/app.js"
+    main: "./src/app.js",
   },
   output: {
     filename: "[name].js",
-    path: path.resolve("./dist")
+    path: path.resolve("./dist"),
   },
   devServer: {
+    /* contentBase: path.join(__dirname, "dist"),
+    publicPath: "/", */
     overlay: true,
     stats: "errors-only",
     proxy: {
-      "/api": "http://localhost:8081"
+      "/api": "http://localhost:8081",
     },
     // TODO: 여기에 핫로딩을 설정하세요
+    // 핫로딩은 코드 변화를 인지하고 전체화면을 새로고침하는 웹팩과 다르게 변경된 모듈에 한해서 그 부분만 고쳐주는 기능이다.
+    // 대표적으로 style-loader와 file-loader가 핫로딩을 지원하고 있다.
+    hot: true,
   },
   module: {
     rules: [
@@ -30,46 +37,46 @@ module.exports = {
             ? MiniCssExtractPlugin.loader // 프로덕션 환경
             : "style-loader", // 개발 환경
           "css-loader",
-          "sass-loader"
-        ]
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
         loader: "url-loader",
         options: {
           name: "[name].[ext]?[hash]",
-          limit: 10000 // 10Kb
-        }
+          limit: 10000, // 10Kb
+        },
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader" // 바벨 로더를 추가한다
-      }
-    ]
+        loader: "babel-loader", // 바벨 로더를 추가한다
+      },
+    ],
   },
   plugins: [
     new webpack.BannerPlugin({
-      banner: `빌드 날짜: ${new Date().toLocaleString()}`
+      banner: `빌드 날짜: ${new Date().toLocaleString()}`,
     }),
     new webpack.DefinePlugin({}),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       templateParameters: {
-        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "",
       },
       minify:
         process.env.NODE_ENV === "production"
           ? {
               collapseWhitespace: true, // 빈칸 제거
-              removeComments: true // 주석 제거
+              removeComments: true, // 주석 제거
             }
           : false,
-      hash: process.env.NODE_ENV === "production"
+      hash: process.env.NODE_ENV === "production",
     }),
     new CleanWebpackPlugin(),
     ...(process.env.NODE_ENV === "production"
       ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
-      : [])
-  ]
+      : []),
+  ],
 };
